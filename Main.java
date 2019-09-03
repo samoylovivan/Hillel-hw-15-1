403d0aa43a09944bc4a433f1c9674b7c2b4ccc12
samoylovivan <52442440+samoylovivan@users.noreply.github.com>
2019-09-03 16:17:38 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,34 @@
+package ivan.samoylov;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        final String FILE = "sample.json";
+        BufferedReader reader = null;
+        String strTemp;
+        String textJson = "";
+
+        try{
+            reader = new BufferedReader(new FileReader(FILE));
+            while((strTemp = reader.readLine()) != null){
+                  textJson += strTemp;
+                }
+            }
+        catch(Exception error){
+            System.out.println(error.getMessage());
+        }
+
+        JsonObject convertedObject = new Gson().fromJson(textJson, JsonObject.class);
+        System.out.println(convertedObject.get("name").getAsString());
+        for (JsonElement obj: convertedObject.get("persons").getAsJsonArray()) {
+            System.out.print(obj.getAsJsonObject().get("name").getAsString());
+            System.out.println(" - " + obj.getAsJsonObject().get("age").getAsString());
+        }
+    }
+}
